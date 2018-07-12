66eaa65acb05daeb297c41577af04ecc38896f85
dtqumak <dtqumak@yandex.com>
2018-07-12 10:53:25 +0200

    poprawka w __init__.py

---
+++ dvik_print/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf8 -*-
+from __future__ import division, absolute_import, print_function, unicode_literals
+
+from .dvik_print import PrettyPrint
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dvik-print',
-    version='0.1',
+    version='0.2',
     description='Funkcja ładnie wypisująca obiekty z możliwością ustawienia wielkości wcięć i limitu pokazywanych kluczy/elementów.',
     url='https://github.com/aquarioos/dvik-print',
     author='Daniel Taranta',
@@ -10,5 +10,5 @@ setup(
     license='MIT',
     packages=['dvik_print'],
     zip_safe=False,
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    python_requires='==2.7, ==3.6',
 )
